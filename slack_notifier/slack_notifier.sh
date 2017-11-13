e8d6ab72ad6429fb08e9135c0be3274ddcb84fcc
eifinger <eifinger@de.ibm.com>
2017-11-13 16:54:26 +0100

    Added file upload if message would lead to "Argument list too long"

--- slack_notifier/slack_notifier.sh
+++ slack_notifier/slack_notifier.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+TEMP_FILE="tmp.txt"
 DATA=$(cat)
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cd $scriptDir
 source ../venv/bin/activate
+LC_ALL=C
+size=$(( ${#DATA} + 1 ))
+# Number taken from here https://stackoverflow.com/a/18647755
+if [ $size -gt 250]; then
+  echo $DATA > $TEMP_FILE
+  python slack_notifier.py -f "$TEMP_FILE" -m "New large message as file"
+fi
 python slack_notifier.py -m "$DATA"
