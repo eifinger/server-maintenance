0a527bcf6b6022b7108f9b48d57fab07cd82bb91
eifinger <eifinger@de.ibm.com>
2017-11-13 11:31:16 +0100

    Refactored shutdown

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+cd $scriptDir
 now=$(date +"%Y-%m-%d.%H:%M:%S")
 hostname=$(hostname)
-source /home/eifinger/maintenance/venv/bin/activate
-python /home/eifinger/maintenance/ubuntu_notifier.py -m "$hostname going down at $now"
+source venv/bin/activate
+python slack_notifier/slack_notifier.py -m "$hostname going down at $now"
