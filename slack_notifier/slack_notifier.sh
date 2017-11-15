2ae0fb6293dd224443e9f2289833bbd7151a3b96
eifinger <eifinger@de.ibm.com>
2017-11-15 10:39:02 +0100

    Wrongly closed paranthesis in slack_notfier.sh

--- slack_notifier/slack_notifier.sh
+++ slack_notifier/slack_notifier.sh
@@ -7,7 +7,7 @@ source ../venv/bin/activate
 LC_ALL=C
 size=$(( ${#DATA} + 1 ))
 # Number taken from here https://stackoverflow.com/a/18647755
-if [ $size -gt 250]; then
+if [ $size -gt 250 ]; then
   echo $DATA > $TEMP_FILE
   python slack_notifier.py -f "$TEMP_FILE" -m "New large message as file"
 fi
